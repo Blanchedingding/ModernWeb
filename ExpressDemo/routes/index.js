f634fcd327556618a7c68f2232c2a27121512aec
Blanchedidngding <18321716610@163.com>
2017-10-12 19:23:45 +0800

    test1 to test2

--- ExpressDemo/routes/index.js
+++ ExpressDemo/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// global.jQuery = require('jquery');
+// require('bootstrap');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
--- ExpressDemo/routes/test2.js
+++ ExpressDemo/routes/test2.js
@@ -2,16 +2,10 @@
  * Created by dingding on 2017/10/11.
  */
 var express = require('express');
-var querystring = require('querystring');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-    var body = "";
-    req.on('data', function (chunk) {
-        body += chunk;
-    });
-    body = querystring.parse(body);
-    res.render('test2', {testName: body.testName});
+    res.render('test2', {testName: req.body.testName});
 });
 
 module.exports = router;
